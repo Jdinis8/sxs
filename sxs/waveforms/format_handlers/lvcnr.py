81c6706c83a9416b6f3742e6263c172a18f1911b
Jdinis8 <joaodinis01@tecnico.ulisboa.pt>
2025-12-25 20:50:05 +0000

    Adding draft of docstring to lvcnr format handler

--- sxs/waveforms/format_handlers/lvcnr.py
+++ sxs/waveforms/format_handlers/lvcnr.py
@@ -6,6 +6,18 @@ import spherical_functions as sf
 from ... import waveforms
 
 def find_nrtimes(f):
+    """Helps the load function to understand how is the top level group "nrtimes" capitalized in the file. Sometimes it is written as "NRtimes", other as "NRTimes", etc. This function searches for it regardless of capitalization and returns its content as a numpy array of float64.
+    
+    Parameters
+    ----------
+    f : h5py.File
+        Opened HDF5 file object.
+
+    Returns
+        A numpy array of float64.
+    """
+    
+    # Array to store the possible matches with the word "nrtimes" in any capitalization
     matches = []
 
     def visitor(name, obj):
@@ -15,32 +27,45 @@ def find_nrtimes(f):
             matches.append(np.array(obj[:], dtype=np.float64))
             return 1  # stop reading the file further
 
+    # Visit all items in the HDF5 file
     f.visititems(visitor)
 
+    # If the amount of times "nrtimes" shows up is different from one, we raise an error because this indicates a problem. If it is zero, it means it was not found. If it is more than one, it means there is ambiguity.
     if len(matches) == 0:
         raise RuntimeError("nrtimes not found in file")
     if len(matches) > 1:
         raise RuntimeError(f"Multiple nrtimes datasets found ({len(matches)})")
 
+    # Return the single found dataset
     return matches[0]
 
 def load(file_name):
-    # This function is a draft to read strain waveforms from the RIT and MAYA waveform catalogs.
+    """Load a waveform from an HDF5 file, in the LVCNR format. This is adapted to read strain data only, but can be extended to read other types of data as well. This was tested with the RIT and MAYA catalogs.
+
+    Args:
+        file_name (str): Path to the HDF5 file.
+
+    Returns:
+        waveforms.WaveformModes: The loaded waveform in WaveformModes format.
+    """
 
     phase_re = re.compile("phase_l(?P<ell>.*)_m(?P<m>.*)")
     amp_re = re.compile("amp_l(?P<ell>.*)_m(?P<m>.*)")
-
-    #Set up default time key, but adapt to the possibility that it is lower case in some files
                 
     with h5py.File(file_name, "r") as f:
-        t = find_nrtimes(f)
         
+        # Find the time array
+        t = find_nrtimes(f)
+
+        # Extract l and m values from the dataset keys
         ell_m = np.array(
             [[int(match["ell"]), int(match["m"])] for key in f for match in [phase_re.match(key)] if match]
         )
         ell_min = np.min(ell_m[:, 0])
         ell_max = np.max(ell_m[:, 0])
         data = np.empty((t.size, sf.LM_total_size(ell_min, ell_max)), dtype=complex)
+        
+        # Compose the data array using the amplitude and phase datasets
         for ell in range(ell_min, ell_max + 1):
             for m in range(-ell, ell + 1):
                 amp = Spline(
@@ -60,7 +85,7 @@ def load(file_name):
             time_axis=0,
             modes_axis=1,
             frame=[],  
-            spin_weight=-2, #checked with scri code
+            spin_weight=-2,
             data_type="h",
             frame_type="inertial",
             history=history,
